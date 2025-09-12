1a6bf9e07e4f88b12ef11157a255ea231ceef97d
e-uphoria <nushojha@gmail.com>
2025-09-12 13:25:40 +0545

    Created 4 tabs in form

--- coffee_help_center/models/help_article.py
+++ coffee_help_center/models/help_article.py
@@ -6,10 +6,45 @@ class HelpCenterArticle(models.Model):
     _order = 'sequence, id'
 
     name = fields.Char(string='Title', required=True)
-    content = fields.Html(string='Content', sanitize=True)
     category_id = fields.Many2one('coffee.helpcenter.category', string='Category')
     sequence = fields.Integer(string='Sequence', default=10)
     published = fields.Boolean(string='Published', default=True)
+
+    # Rich HTML content per tab
+    about_html = fields.Html(string='About the Module', sanitize=False)
+    howto_html = fields.Html(string='How to Use', sanitize=False)
+    c4_html = fields.Html(string='C4 Model Diagram', sanitize=False)
+    faq_html = fields.Html(string='FAQ', sanitize=False)
+
+    # Attachments per tab with explicit relation tables
+    about_attachment_ids = fields.Many2many(
+        'ir.attachment', 
+        'help_article_about_rel', 
+        'article_id', 
+        'attachment_id', 
+        string='About Files'
+    )
+    howto_attachment_ids = fields.Many2many(
+        'ir.attachment', 
+        'help_article_howto_rel', 
+        'article_id', 
+        'attachment_id', 
+        string='How-To Files'
+    )
+    c4_attachment_ids = fields.Many2many(
+        'ir.attachment', 
+        'help_article_c4_rel', 
+        'article_id', 
+        'attachment_id', 
+        string='C4 Files'
+    )
+    faq_attachment_ids = fields.Many2many(
+        'ir.attachment', 
+        'help_article_faq_rel', 
+        'article_id', 
+        'attachment_id', 
+        string='FAQ Files'
+    )
 
 
 class HelpCenterCategory(models.Model):
